2416f1f01f2b24398351bcb76983cec233138f2e
Tulio <tulio.ramirez@fizzmod.com>
2021-05-27 15:05:39 -0300

    changed LinkedinApp file

--- src/LinkedinApp.js
+++ src/LinkedinApp.js
@@ -1,10 +1,6 @@
 import React from "react";
 import AppRouter from "./routers/AppRouter";
 
-const LinkedinApp = () => (
-  <>
-    <AppRouter />
-  </>
-);
+const LinkedinApp = () => <AppRouter />;
 
 export default LinkedinApp;
